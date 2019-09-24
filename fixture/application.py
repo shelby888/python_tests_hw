0006186fa88bf0068a7646e2ab1f507f422bf92f
Ivan Subbotin <ivan.subbotin@rt.ru>
2019-09-24 13:11:33 +0400

    выделен класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,49 +1,15 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init creation
-        wd.find_element_by_name("new").click()
-        # fill forms
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit creaton
-        wd.find_element_by_name("submit").click()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,14 +11,14 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="test 1", header="adasdasdsd", footer="hdscnjs kjsd"))
-    app.return_to_groups_page()
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="test 1", header="adasdasdsd", footer="hdscnjs kjsd"))
+    app.group.return_to_groups_page()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
-    app.return_to_groups_page()
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="", header="", footer=""))
+    app.group.return_to_groups_page()
+    app.session.logout()
