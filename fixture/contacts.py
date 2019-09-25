1a49eeb31a787d617997e2d48bfb3dc7136a082e
Ivan Subbotin <ivan.subbotin@rt.ru>
2019-09-25 18:18:15 +0400

    добавил редактирование контакта

--- fixture/contacts.py
+++ fixture/contacts.py
@@ -22,19 +22,25 @@ class ContactsHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
 
-    def update_first_group(self, group):
+    def open_contact_page(self):
         wd = self.app.wd
-        self.open_groups_page()
-        # select first
+        wd.find_element_by_link_text("home").click()
+
+    def update_first_contact(self, contact):
+        wd = self.app.wd
+        # init creation
+        self.open_contact_page()
         wd.find_element_by_css_selector("img[alt=\"Edit\"]").click()
-        # edit
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # fill forms
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstName)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastName)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.homePhone)
         # submit
         wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
+        self.return_to_contacts_page()
