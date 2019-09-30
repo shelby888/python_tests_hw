4f43eddf3ba91d33b931576695c4ae8a0b84bca1
Ivan Subbotin <ivan.subbotin@rt.ru>
2019-09-30 15:58:31 +0400

    добавил возможность редактирования отдельных полей в группе

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -7,3 +7,14 @@ def test_add_group(app):
     app.group.update_first_group(Group(name="Updated", header="Updated header", footer="updated footer"))
     app.session.logout()
 
+
+def test_update_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.update_first_group(Group(name="Updated"))
+    app.session.logout()
+
+
+def test_update_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.update_first_group(Group(header="Updated header"))
+    app.session.logout()
