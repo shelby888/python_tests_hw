77b19766cf6961a929ac52c04c0ccaba3e8fc7f1
Ivan Subbotin <ivan.subbotin@rt.ru>
2019-10-01 17:27:56 +0400

    добавил проверку на отсутствие групп при обновлении и удалении группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,12 +3,18 @@ from model.group import Group
 
 
 def test_add_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name='test'))
     app.group.update_first_group(Group(name="Updated", header="Updated header", footer="updated footer"))
 
 
 def test_update_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name='test'))
     app.group.update_first_group(Group(name="Updated"))
 
 
 def test_update_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name='test'))
     app.group.update_first_group(Group(header="Updated header"))
