5ae94080c354f2d859cd73f1aa9180fa80f5258f
Ivan Subbotin <ivan.subbotin@rt.ru>
2019-10-19 00:07:42 +0400

    add test_address_on_contact_view_page

--- test/test_contact_data.py
+++ test/test_contact_data.py
@@ -25,7 +25,13 @@ def test_address_on_home_page(app):
     contact_from_edit_page = app.contacts.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.address == contact_from_edit_page.address
 
-
+def test_address_on_contact_view_page(app):
+    app.contacts.is_contact_empty_create_contact()
+    contacts_list = app.contacts.get_contact_list()
+    index = randrange(len(contacts_list))
+    contact_from_view_page = app.contacts.get_full_text_from_view_page(index)
+    contact_from_edit_page = app.contacts.get_contact_info_from_edit_page(index)
+    assert contact_from_view_page.find(contact_from_edit_page.address) != -1
 
 def merge_emails_like_hp(contact):
     return "\n".join(filter(lambda x: x != "",
