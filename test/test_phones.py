15f2cc39eeadc799f145d5f1566690d1b4d7896b
Ivan Subbotin <ivan.subbotin@rt.ru>
2019-10-18 20:05:59 +0400

    добавил случайны выбор контакта при тестировании телефона

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,15 +1,25 @@
 import re
+from model.contact import Contact
+from random import randrange
 
 
 def test_phones_on_home_page(app):
-    contact_from_home_page = app.contacts.get_contact_list()[0]
-    contact_from_edit_page = app.contacts.get_contact_info_from_edit_page(0)
+    if app.contacts.count() == 0:
+        app.contacts.create_contact(Contact(firstName='test'))
+    contacts_list = app.contacts.get_contact_list()
+    index = randrange(len(contacts_list))
+    contact_from_home_page = contacts_list[index]
+    contact_from_edit_page = app.contacts.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_phones_frome_hp == merge_phones_like_hp(contact_from_edit_page)
 
 
 def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contacts.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contacts.get_contact_info_from_edit_page(0)
+    if app.contacts.count() == 0:
+        app.contacts.create_contact(Contact(firstName='test'))
+    contacts_list = app.contacts.get_contact_list()
+    index = randrange(len(contacts_list))
+    contact_from_view_page = app.contacts.get_contact_from_view_page(index)
+    contact_from_edit_page = app.contacts.get_contact_info_from_edit_page(index)
     assert contact_from_view_page.homePhone == contact_from_edit_page.homePhone
     assert contact_from_view_page.workphone == contact_from_edit_page.workphone
     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
